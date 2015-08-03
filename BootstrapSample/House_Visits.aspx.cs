16dc4b0d074c95aa2d939c011e8a621d61d4fb46
Rachel Griffiths <thisisrachelalice@hotmail.com>
2015-08-03 15:00:15 +0100

    house visit inherit fixed

--- BootstrapSample/House_Visits.aspx.cs
+++ BootstrapSample/House_Visits.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace BootstrapSample
 {
-    public partial class Dog_Cat_Sitting : System.Web.UI.Page
+    public partial class House_Visits : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
